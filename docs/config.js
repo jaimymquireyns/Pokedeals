0ceff7cb0e7b473bea9b4829a489e556ebfb4992
jaimymquireyns <jaimy.m.quireyns@gmail.com>
2026-09-24 08:32:33 +0200

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 window.POKEDEALS = {
   SUPABASE_URL: "https://blvikvqbnxfssrlydzof.supabase.co",
-  SUPABASE_KEY: "jouw_publishable_of_anon_key",
-  VAPID_PUBLIC_KEY: "sb_publishable_fTN8tnB4xMnn2Vjg5Ibu4w_EtSPr3l9",
+  SUPABASE_KEY: "sb_publishable_fTN8tnB4xMnn2Vjg5Ibu4w_EtSPr3l9",
+  VAPID_PUBLIC_KEY: "",
 };
